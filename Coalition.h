3291f3759088de29545cc7ffd289d265f100bf16
roeeziv <102467210+roeeziv@users.noreply.github.com>
2022-11-21 13:38:58 +0200

    Update Coalition.h

--- Coalition.h
+++ Coalition.h
@@ -7,14 +7,15 @@ using std::string;
 class Coalition
 {
 public:
-    Coalition(int id , int currMandates, int size ) : coalitionID(id) , numOfMandates(currMandates) , offersCoalitionMade({}); //constructor  (check if we initialize bool array like that)
+    Coalition(int id , int currMandates ) : coalitionID(id) , numOfMandates(currMandates) , offersCoalitionMade = { 0 }; //constructor  (check if we initialize bool array like that)
     void coalitionJoin(int PartyID) ; //gets the party ID that join the coalition and updates: numOfMandates, BiggestCoalition(for JoinPolicy),
                               // counterJoined (checks how many parties already joined (for 'shouldTerminate method) .
 
 private:
     int coalitionID ;
     int numOfMandates ;
-    vector<bool> offersCoalitionMade;  //if the coalition made an offer to 'i' party , then offersCoalitionMade[i]=true    --->TAMAR
+    //bool offersCoalitionMade[getNumVertices] ;  //if the coalition made an offer to 'i' party , then offersCoalitionMade[i]=true    --->TAMAR
+
     vector<Party&> coalitionParties ;
 };
 
