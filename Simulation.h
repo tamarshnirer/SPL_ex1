b8b56aa2066c8beeb0afb784fffdd72520558bd7
roeeziv <102467210+roeeziv@users.noreply.github.com>
2022-11-21 13:39:42 +0200

    Update Simulation.h

--- Simulation.h
+++ Simulation.h
@@ -20,9 +20,20 @@ public:
     const vector<Agent> &getAgents() const;
     const Party &getParty(int partyId) const;
     const vector<vector<int>> getPartiesByCoalitions() const;
-    void addAgent(Agent newAgent);
+
+
+//should add all methods for party and agents   ----> Tamar
+
 
 private:
     Graph mGraph;
     vector<Agent> mAgents;
+
+    //two fields that give information about the terminates:
+    int counterJoined ;  //checks how many parties already joined  --> needs to be initialized at the beginning of the simulation.
+    int biggestCoalition ; //checks what is the number of mandates of the biggest coalition
+
+    bool joinedParties[getNumVertices]; // initialize the simulation with the parties that already joined to coalition  ---> maybe vector better
+    int numOfParties;
+    vector<Coalition> coalitions;
 };
